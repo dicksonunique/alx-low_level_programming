8d40f1149437a08da0139c2a871169ee5e89bc79
Uwem Dickson <dicksonunique@gmail.com>
2022-09-10 09:29:43 -0700

    Cprograme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point
- * Description: Prints strings
- * Result: 1
- */	
+ * main - prints to string
+ *
+ * Description: Prints "and that piece of art is useful.." without puts
+ *
+ * Result: Always return 1
+ */
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
