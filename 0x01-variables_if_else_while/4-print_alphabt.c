23c92884a70752293d2f391b364c148c46998eff
dicksonunique <dicksonunique@gmail.com>
2022-09-12 21:13:46 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (chr != 'e' && chr != 'e')
+		{
+			putchar(chr);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
