8382736635931c70e355306431e7771c454b4fdd
Uwem Dickson <dicksonunique@gmail.com>
2022-09-12 17:12:18 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = '0', k < '10'; k++)
+	{
+		putchar(k);
+
+		if (k != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
