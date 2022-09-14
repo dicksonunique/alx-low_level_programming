0eb466d173d77ebcf65814a278e34542033b262c
Uwem Dickson <dicksonunique@gmail.com>
2022-09-14 16:25:01 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Print putchar entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('U');
+	_putchar('W');
+	_putchar('E');
+	_putchar('M');
+	_putchar('\n');
+	return (0);
+}
