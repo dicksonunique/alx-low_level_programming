cb3ab95f9505ad2349286ac83d25c49cb77d2d43
Uwem Dickson <dicksonunique@gmail.com>
2022-09-16 15:46:31 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,7 @@ void times_table(void)
 		{
 			result = i * j;
 			if (result == 0)
-			{
-				
-				_putchar(' ');
-				_putchar(' ');
+			
 				_putchar('0');
 			}
 			else if (result < 10 && result != 0)
