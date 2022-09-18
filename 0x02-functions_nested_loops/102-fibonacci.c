38c44e1cfacab68174f72099cc3a8c66ab63ab85
Uwem Dickson <dicksonunique@gmail.com>
2022-09-18 09:15:49 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	int term1 = 1, term2 = 2;
-	int i, next_term;
+	unsigned long term1 = 1, term2 = 2, next_term;
+	int i;
 
 	for (i = 0; i <= 50; i++)
 	{
 		next_term = term1 + term2;
-		printf("%d", next_term);
+		printf("%lu", next_term);
 		term1 = term2;
 		term2 = next_term;
 		if (i == 49)
