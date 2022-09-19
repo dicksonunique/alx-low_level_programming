5648fbd4dc802cc1d668eb0d5d986cd704eac97c
dicksonunique <dicksonunique@gmail.com>
2022-09-19 23:39:10 +0000

    C advanced code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - print reverse string
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	while (s[i++])
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar("\n");
+}
