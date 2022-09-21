ca3895f4ce4008fedd8aaf9a46d2b4dff0663c3c
Uwem Dickson <dicksonunique@gmail.com>
2022-09-21 03:28:18 +0000

    keygen generation

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-	int p[110];
+	int p[100];
 	int i, s, j;
 
 	s = 0;
 	srand(time(NULL));
 
-	for (i = 0; i < 110; i++)
+	for (i = 0; i < 100; i++)
 	{
-		p[i] = rand() % 88;
+		p[i] = rand() % 78;
 		s = s + (p[i] + '0');
 		putchar(p[i] + '0');
-		if ((2772 - s) - '0' < 88)
+		if ((2772 - s) - '0' < 78)
 		{
 			j = 2772 - s - '0';
 			s= s + j;
