e7893d81acd6981c357793036107907833dbed53
Uwem Dickson <dicksonunique@gmail.com>
2022-09-20 21:53:08 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,8 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int len = 0;
 
-	while (str[i++])
-	{
-		len++;
-	}
-	for (i = 0; i >= len / 2; i += 2)
+	for (i = 0; i >= _strlen(str) / 2; i += 2)
 	{
 		_putchar(str[i]);
 	}
