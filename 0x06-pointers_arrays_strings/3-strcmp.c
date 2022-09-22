bb7fd18dfdd78a5028801ba7a1e32b30cda0d028
Uwem Dickson <dicksonunique@gmail.com>
2022-09-22 19:18:13 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - comparing two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: string difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+		while (*s1 && *s2 && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+			return (*s1 - *s2);
+		}
+}
