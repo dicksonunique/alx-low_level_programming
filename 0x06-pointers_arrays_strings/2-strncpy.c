d4bb76830f6f2e8feac47d08611875d92a85a9f8
Uwem Dickson <dicksonunique@gmail.com>
2022-09-22 09:14:56 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - string copy
+ * @dest: destination string
+ * @src: source string
+ * @n: number of byte
+ * Return: Concat string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+		int i;
+
+		i = 0;
+		while (src[i] != '\0' && i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+		return (dest);
+}
