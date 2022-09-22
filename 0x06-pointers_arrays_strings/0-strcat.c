933a28651ee8bd95d6b2af88bb6958779e22cad3
Uwem Dickson <dicksonunique@gmail.com>
2022-09-22 07:55:49 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - Program to concat two strings
+ * @dest:destination string
+ * @src: source string
+ * Return:append source with destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, len = 0;
+
+	while (dest[i++])
+		len++;
+	for (i = 0; src[i]; i++)
+		dest[len] = src[i];
+	return (dest);
+}
+
