5065ddb51a1c17e7d5c3c5735d7c48a4e2718c3f
Uwem Dickson <dicksonunique@gmail.com>
2022-09-22 19:42:23 +0000

    main header

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strcmp - comparing two strings
  * @s1: string 1
@@ -11,6 +12,6 @@ int _strcmp(char *s1, char *s2)
 		{
 			s1++;
 			s2++;
-			return (*s1 - *s2);
 		}
+		return (*s1 - *s2);
 }
