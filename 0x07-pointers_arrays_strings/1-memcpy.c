d4332cc538538d37c880efcf7056fd59d55a00bc
Uwem Dickson <dicksonunique@gmail.com>
2022-09-26 08:04:04 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "mai.h"
+/**
+ * _memcpy - Function to copy memory area
+ * @dest: destination memory to copy to
+ * @src: source memory to copy from
+ * @n: number of byte
+ * Return: pointer to destination(dest)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
