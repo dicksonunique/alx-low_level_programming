b8fc2224f52dac9930f7d8ffa92cc7953b0a40ae
Uwem Dickson <dicksonunique@gmail.com>
2022-10-01 01:35:18 -0700

    using argv to add two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Prints the multiplication of two args numbers
+  * @argc: argument count
+  * @argv: argument array.
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		mul = a * b;
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
