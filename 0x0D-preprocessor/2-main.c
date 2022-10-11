2143fab61d41d1a5b43989995f1e8f5a3f5e7362
Uwem Dickson <dicksonunique@gmail.com>
2022-10-11 22:10:43 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entrance of the code
+ * Description:program that prints the name of the file
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
