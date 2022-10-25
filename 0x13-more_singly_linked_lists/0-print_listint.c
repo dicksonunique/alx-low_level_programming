b82d57737903c974a54849133368ade2849cac3e
Uwem Dickson <dicksonunique@gmail.com>
2022-10-25 09:07:28 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lish.h"
+/**
+*print_listint - display all elements of a list
+*
+*@h: Head of the node
+*Return: node of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	if (h == NULL)
+	{
+		printf("The link is empty");
+	}
+	while (h != NULL)
+	{
+		counter++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (counter);
+}
